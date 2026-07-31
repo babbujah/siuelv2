590bc110df8768722ac2f7d2237a948f3b8147fd
Bruno Lopes <bczion@hotmail.com>
2026-07-31 10:44:00 -0300

    Limpeza de versionamento e configuração de ambiente

--- app/config/siuel_negocio.example.php
+++ app/config/siuel_negocio.example.php
@@ -3,8 +3,8 @@ return [
     'host'  =>  "127.0.0.1",
     'port'  =>  "3306", // vazio usa a porta padrão (3306)
     'name'  =>  "siuelv2negociodb",
-    'user'  =>  "siuel_user",
-    'pass'  =>  "g,2>n23Y5Mo6",
+    'user'  =>  "SEU_USUARIO",
+    'pass'  =>  "SUA_SENHA",
     'type'  =>  "mysql",
     'prep'  =>  "1",
     'char'  =>  "utf8mb4", // Ex. ISO (ISO-8859-1), utf8mb4
